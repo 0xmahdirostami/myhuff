e20a0a3e224c7f88af1606ac630c41c0fa9e3e75
0xmahdirostami <rostamimahdi1997@gmail.com>
2023-07-22 08:50:58 +0330

    update

--- huff-challenges-solution/README.md
+++ huff-challenges-solution/README.md
@@ -4,3 +4,8 @@ Huff challenge [#1](https://twitter.com/huff_language/status/1559658361469095936
 Write the minimal Huff code for a contract that returns the current block number if called
 <br>
 Solution: [challenge1.huff](https://github.com/0xmahdirostami/myhuff/blob/main/huff-challenges-solution/challenge1.huff)
+
+Huff challenge [#2](https://twitter.com/huff_language/status/1560015751989211136):
+Write the most efficient possible Huff contract that returns true (1) if the input is an even number and false (0) if it is odd.
+<br>
+Solution: [challenge2.huff](https://github.com/0xmahdirostami/myhuff/blob/main/huff-challenges-solution/challenge2.huff)
---
+++ myprojects/test/TSOwnable.t.sol
@@ -0,0 +1,94 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+
+import {HuffDeployer} from "foundry-huff/HuffDeployer.sol";
+
+interface ITSOwnable {
+    function owner() external view returns (address);
+    function pendingOwner() external view returns (address);
+
+    function setPendingOwner(address pendingOwner) external;
+    function acceptOwnership() external;
+}
+
+contract TSOwnableTest is Test {
+
+    // The System under Test.
+    ITSOwnable sut;
+
+    // Events copied from SuT.
+    event NewPendingOwner(
+        address indexed previousPendingOwner,
+        address indexed newPendingOwner
+    );
+    event NewOwner(address indexed previousOwner, address indexed newOwner);
+
+    function setUp() public {
+        address impl = HuffDeployer.deploy("TSOwnable");
+        sut = ITSOwnable(impl);
+    }
+
+    function testDeploymentInvariants() public {
+        assertEq(sut.owner(), address(this));
+        assertEq(sut.pendingOwner(), address(0));
+    }
+
+    function testFailSetPendingOwnerToOwner() public {
+        sut.setPendingOwner(address(this));
+    }
+
+    function testFailSetPendingOwnerOnlyCallableByOwner(address caller) public {
+        vm.assume(caller != address(this));
+
+        vm.startPrank(caller);
+        sut.setPendingOwner(address(1));
+    }
+
+    function testSetPendingOwner(address first, address second) public {
+        vm.assume(first != sut.owner());
+        vm.assume(second != sut.owner());
+
+        // Set pending owner to first.
+        vm.expectEmit(true, true, true, true);
+        emit NewPendingOwner(address(0), first);
+
+        sut.setPendingOwner(first);
+        assertEq(sut.pendingOwner(), first);
+
+        // Set pending owner to second.
+        vm.expectEmit(true, true, true, true);
+        emit NewPendingOwner(first, second);
+
+        sut.setPendingOwner(second);
+        assertEq(sut.pendingOwner(), second);
+    }
+
+    function testAcceptOwnership(address pendingOwner) public {
+        address owner = sut.owner();
+
+        vm.assume(pendingOwner != owner);
+
+        sut.setPendingOwner(pendingOwner);
+
+        vm.expectEmit(true, true, true, true);
+        emit NewOwner(owner, pendingOwner);
+
+        vm.prank(pendingOwner);
+        sut.acceptOwnership();
+
+        assertEq(sut.owner(), pendingOwner);
+        assertEq(sut.pendingOwner(), address(0));
+    }
+
+    function testFailAcceptOwnershipOnlyCallableByPendingOwner(address caller)
+        public
+    {
+        vm.assume(caller != sut.pendingOwner());
+
+        vm.startPrank(caller);
+        sut.acceptOwnership();
+    }
+
+}
